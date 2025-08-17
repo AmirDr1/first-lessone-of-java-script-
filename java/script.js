bfa139bd23675374368ebb1d8a9ae0ec2eb42963
AmirDr1 <ppppppwwpt@gmail.com>
2025-08-17 18:38:30 +0330

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,22 @@
+var lastname="hello world";
+
+console.log(lastname) ;
+{
+  var worldname= "what is your name";
+  console.log( worldname) ;
+  {
+    var lastnameworld="name of world is earth" ;
+
+    console.log( lastnameworld) ;
+
+  }
+}
+// consolel.log( );
+
+var lostname="abas boazar";
+
+console.log(lostname);
+
+var listname="yedoen R dare";
+
+console.log(listname);
